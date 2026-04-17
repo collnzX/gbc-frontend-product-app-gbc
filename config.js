2a79c6467223489c409e43a710c53d6aaa6d1c4b
collnzX <113921792+collnzX@users.noreply.github.com>
2026-04-17 13:49:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const APP_CONFIG = {
-  apiUrl: "https://beckend-functionapp-gbc-cyhadbheeyazgben.canadacentral-01.azurewebsites.net/api/getProducts"
+  apiUrl: "https://beckend-functionapp-gbc-cyhadbheeyazgben.canadacentral-01.azurewebsites.net/api/getProducts" 
 };
