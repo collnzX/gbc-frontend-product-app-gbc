494136d3d1ff5010e127415610d00539625bf85f
collnzX <113921792+collnzX@users.noreply.github.com>
2026-04-17 12:54:22 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const APP_CONFIG = {
-  apiUrl: "https://dr-backend-end-function-eqabdbhedyeze3b9.canadaeast-01.azurewebsites.net/api/getProducts"
+  apiUrl: "https://beckend-functionapp-gbc-cyhadbheeyazgben.canadacentral-01.azurewebsites.net/api/getProducts"
 };
